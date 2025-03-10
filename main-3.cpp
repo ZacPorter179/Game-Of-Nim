827d6d2fa1631d82d15f5684027dc1aa6bc28302
Zac Porter <zacporter179@gmail.com>
2025-03-10 14:54:40 -0600

    Update main-3.cpp

--- main-3.cpp
+++ main-3.cpp
@@ -1,58 +1,61 @@
-#include <ctime>
-#include <iostream>
-using namespace std;
+#include <ctime> // Include library for time-based functions
+#include <iostream> // Include library for input and output operations
+using namespace std; // Use the standard namespace
+
+// Function to determine the computer's choice of toothpicks
 int computerChoice(int remainingToothpicks) {
-  if (remainingToothpicks == 1) {
-    return 1;
-  } else if (remainingToothpicks >= 2 && remainingToothpicks <= 4) {
-    return remainingToothpicks - 1;
-  } else if (remainingToothpicks >= 6 && remainingToothpicks <= 8) {
-    return remainingToothpicks - 5;
+  if (remainingToothpicks == 1) { // If only one toothpick remains
+    return 1; // Take the last toothpick
+  } else if (remainingToothpicks >= 2 && remainingToothpicks <= 4) { // If 2 to 4 toothpicks remain
+    return remainingToothpicks - 1; // Take enough to leave 1 for the opponent
+  } else if (remainingToothpicks >= 6 && remainingToothpicks <= 8) { // If 6 to 8 toothpicks remain
+    return remainingToothpicks - 5; // Take enough to leave 5 toothpicks
   } else {
-    int x = (rand() % 3) + 1;
-    return (4 * x) % 3 + 1;
+    int x = (rand() % 3) + 1; // Generate a random number between 1 and 3
+    return (4 * x) % 3 + 1; // Compute the number of toothpicks to take
   }
 }
+
+// Main function
 int main() {
+  srand(time(0)); // Seed the random number generator with current time
 
-  srand(time(0));
+  int toothPicks = 23; // Initialize the total number of toothpicks
+  string playerName; // Variable to store the player's name
+  int playerPick; // Variable to store the player's choice of toothpicks
+  int computerPick; // Variable to store the computer's choice of toothpicks
 
-  int toothPicks = 23;
-  string playerName;
-  int playerPick;
-  int computerPick;
+  cout << "Welcome to the 23 Toothpick Game!\n"; // Display game introduction
+  cout << "Please enter your name:\n"; // Prompt the player for their name
+  cin >> playerName; // Read the player's name from input
 
-  cout << "Welcome to the 23 Toothpick Game!\n";
-  cout << "Please enter your name:\n";
-  cin >> playerName;
+  while (toothPicks > 0) { // Loop until all toothpicks are taken
+    cout << "There are " << toothPicks << " toothpicks left.\n"; // Display remaining toothpicks
+    cout << "How many toothpicks will you take out? (1, 2, or 3)\n"; // Ask for player's move
+    cin >> playerPick; // Read the player's choice
 
-  while (toothPicks > 0) {
-    cout << "There are " << toothPicks << " toothpicks left.\n";
-    cout << "How many toothpicks will you take out? (1, 2, or 3)\n";
-    cin >> playerPick;
-
-    if (playerPick < 1 || playerPick > 3 || playerPick > toothPicks) {
-      cout << "Invalid input. Please try again.\n";
-      continue;
+    if (playerPick < 1 || playerPick > 3 || playerPick > toothPicks) { // Validate input
+      cout << "Invalid input. Please try again.\n"; // Display error message
+      continue; // Restart the loop
     }
-    toothPicks -= playerPick;
+    toothPicks -= playerPick; // Subtract player's pick from total toothpicks
 
-    if (toothPicks == 0) {
+    if (toothPicks == 0) { // If no toothpicks remain
       cout << "Sorry, " << playerName
-           << " you lost to a dang computer! Better luck next time!\n";
-      break;
+           << " you lost to a dang computer! Better luck next time!\n"; // Display loss message
+      break; // Exit the loop
     }
-    computerPick = computerChoice(toothPicks);
-    cout << "The computer took out " << computerPick << " toothpicks.\n";
+    computerPick = computerChoice(toothPicks); // Get computer's move
+    cout << "The computer took out " << computerPick << " toothpicks.\n"; // Display computer's move
 
-    toothPicks -= computerPick;
+    toothPicks -= computerPick; // Subtract computer's pick from total toothpicks
 
-    if (toothPicks == 0) {
-      cout << "Congradulations, " << playerName
-           << " ! You beat the computer!\n";
-      break;
+    if (toothPicks == 0) { // If no toothpicks remain
+      cout << "Congratulations, " << playerName
+           << " ! You beat the computer!\n"; // Display win message
+      break; // Exit the loop
     }
   }
-  cout << "Game Over. Thanks for playing!\n";
-  return 0;
+  cout << "Game Over. Thanks for playing!\n"; // Display game over message
+  return 0; // Return successful execution status
 }
